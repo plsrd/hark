c5c64bac63d2c63a2375b2a7ec26a0ee77df3b74
Racheal Pennell <racheal.larimer@gmail.com>
2022-08-08 13:53:46 -0700

    create Role schema

--- api/models/Post.js
+++ api/models/Post.js
@@ -6,6 +6,7 @@ const PostSchema = new Schema({
   title: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  content: [{ type: Object }],
   timestamps: true,
 });
 
