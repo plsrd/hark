10cc0d67593e9abf6b3a2f1e7859c82276f20009
Racheal Pennell <racheal.larimer@gmail.com>
2022-08-08 13:23:27 -0700

    add Post schema

---
+++ api/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Post', PostSchema);
