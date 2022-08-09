40bfed893af20e63d1cf1845263a9f178c9f6ccd
Racheal Pennell <racheal.larimer@gmail.com>
2022-08-08 17:13:30 -0700

    add user_get function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,15 +5,19 @@ const validation = require('../middleware/validation');
 const createNewUser = require('../middleware/createNewUser');
 
 exports.users_get = async (req, res, next) => {
-  const users = await User.find();
-
-  res.json({ users });
+  await User.find()
+    .then(users => res.json({ users }))
+    .catch(err => {
+      if (err) next(err);
+    });
 };
 
 exports.users_post = [...validation.newUserValidation, createNewUser];
 
-exports.user_get = (req, res, next) => {
-  res.json({ message: 'User Found' });
+exports.user_get = async (req, res, next) => {
+  await User.findById(req.params.userid)
+    .then(user => res.json({ user }))
+    .catch(err => next(err));
 };
 
 exports.user_put = (req, res, next) => {
