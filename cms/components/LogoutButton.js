76c0858b6703f12712b58ac4552096fd933212a1
Racheal Pennell <racheal.larimer@gmail.com>
2022-08-22 18:55:50 -0700

    add redirect to logout button

--- cms/components/LogoutButton.js
+++ cms/components/LogoutButton.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
+import { useRouter } from 'next/router'
 import UserContext from '../src/userContext';
 import client from '../src/client';
 
 const LogoutButton = () => {
   const { setUser } = useContext(UserContext);
+  const router = useRouter()
 
   const handleClick = () => {
     client.logout();
     setUser();
+    router.push('/')
   };
 
   return <button onClick={handleClick}> Log Out </button>;
